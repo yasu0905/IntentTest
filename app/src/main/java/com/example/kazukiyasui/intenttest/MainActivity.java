e0e57783d2f92d6a1015760e1e3ce4ac5c134391
yasu0905 <35673575+yasu0905@users.noreply.github.com>
2018-07-25 15:34:37 +0900

    画面遷移テスト用

--- app/src/main/java/com/example/kazukiyasui/intenttest/MainActivity.java
+++ app/src/main/java/com/example/kazukiyasui/intenttest/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.kazukiyasui.intenttest;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String packageName = getPackageName();
+
+        //「遷移1」ボタン
+        Button bt_toSub = findViewById(R.id.btn_toSub);
+        bt_toSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName(packageName, packageName + ".SubActivity");
+                startActivity(intent);
+            }
+        });
+
+        //「遷移2」ボタン
+        Button bt_toSub2 = findViewById(R.id.btn_toSub2);
+        bt_toSub2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName(packageName, packageName + ".SubActivity");
+
+                EditText et_message = findViewById(R.id.et_Message);
+                String message = et_message.getText().toString();
+                intent.putExtra("message", message);
+
+                startActivity(intent);
+            }
+        });
+
+        //「遷移3」ボタン
+        Button bt_toSub3 = findViewById(R.id.btn_toSub3);
+        bt_toSub3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName(packageName, packageName + ".SubActivity");
+
+                //画面遷移（戻ってくること前提）
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == 0) {
+            String btn_str = "";
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    btn_str = data.getStringExtra("button_text");
+                    break;
+                case Activity.RESULT_CANCELED:
+                    btn_str = "キャンセルされました。";
+                    break;
+            }
+            Toast.makeText(this, btn_str, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/kazukiyasui/intenttest/SubActivity.java
+++ app/src/main/java/com/example/kazukiyasui/intenttest/SubActivity.java
@@ -1,13 +1,57 @@
 package com.example.kazukiyasui.intenttest;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SubActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity implements View.OnClickListener {
+    Intent intent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
 
+        //遷移元からのデータを取得
+        intent = getIntent();
+        String message = intent.getStringExtra("message");
+
+        if (message != null) {
+            //メッセージ出力
+            Toast.makeText(SubActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+
+        //「A」ボタン
+        Button bt_a = findViewById(R.id.btn_A);
+        bt_a.setOnClickListener(this);
+
+        //「B」ボタン
+        Button bt_b = findViewById(R.id.btn_B);
+        bt_b.setOnClickListener(this);
+
+        //「Cancel」ボタン
+        Button bt_cancel = findViewById(R.id.btn_Cancel);
+        bt_cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_A:
+            case R.id.btn_B:
+                intent.putExtra("button_text", ((Button)view).getText());
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+                break;
+            case R.id.btn_Cancel:
+                setResult(Activity.RESULT_CANCELED, intent);
+                finish();
+                break;
+        }
     }
 }
